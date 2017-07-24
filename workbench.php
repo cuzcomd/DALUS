1492bfb58410c5d5669b9dabb3310d9a37fa2de5
cuzcomd <m.trott@web.de>
2017-07-24 16:03:09 +0200

    Suche setzte Marker
    Suche setzt an einen marker an die ermittelste Koordinate

--- workbench.php
+++ workbench.php
@@ -214,38 +214,57 @@ function updateAllUsers(){ //Aktualisiert die Liste der Projekte, die für den a
 			deleteProject();	
 		});
 
-        var input = (document.getElementById('pac-input'));
+		var input = (document.getElementById('pac-input'));
 
         var autocomplete = new google.maps.places.Autocomplete(input);
         autocomplete.bindTo('bounds', map);
 
         autocomplete.addListener('place_changed', function() {
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-          }
+        	var place = autocomplete.getPlace();
+        	if (!place.geometry) {
+           		new google.maps.Geocoder().geocode( { 'address': place.name}, function(results, status) {
+		    		if (status == 'OK') {
+				        map.setCenter(results[0].geometry.location);
+				        var marker = new google.maps.Marker({
+				            map: map,
+				            position: results[0].geometry.location
+				        });
+			      	} 
+			      	else {
+     					alert('Geocode was not successful for the following reason: ' + status);
+     				}
+     			});
+     			 return;
+     		}
 
-          // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
-          }
 
-          var address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
+	        var marker = new google.maps.Marker({
+	          	map: map,
+				position: place.geometry.location
+			});
+	        // If the place has a geometry, then present it on a map.
+	        if (place.geometry.viewport) {
+	        	map.fitBounds(place.geometry.viewport);
+	        } 
+	        else {
+	            map.setCenter(place.geometry.location);
+	            map.setZoom(17);  // Why 17? Because it looks good.
+	        }
+
+	        var address = '';
+	        if (place.address_components) {
+	            address = [
+	            (place.address_components[0] && place.address_components[0].short_name || ''),
+	            (place.address_components[1] && place.address_components[1].short_name || ''),
+	            (place.address_components[2] && place.address_components[2].short_name || '')
+	            ].join(' ');
+	        }
         });
+
+
+        
 	}//Ende Funktion initMap
+
 	</script> <!-- Initialfunktion -->
 	<script > // Google DrawingManager laden
 	function startDrawingManager(map){
@@ -1016,7 +1035,7 @@ function updateAllUsers(){ //Aktualisiert die Liste der Projekte, die für den a
 			<div id ="suche" class="tab-pane fade">
 				<div class="input-group">
 					<input id="pac-input" class="form-control" type="text" placeholder="Ort suchen ...">
-					<span class="input-group-addon"><i class="fa fa-search"></i></span>
+					<span class="input-group-addon" id="startSuche"><i class="fa fa-search"></i></span>
 				</div>
 			</div>
 		</div>	<!-- Ende Tab_content -->
